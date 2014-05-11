13461309fe5392488a2ec2b82de1a8f378751619
Will Franzen <wtfranzen@gmail.com>
2014-05-10 19:37:54 -0500

    Initial commit

---
+++ StudentID.js
@@ -0,0 +1,37 @@
+module.exports = function(id) {
+    if((id+'').length != 7)
+        throw('Invaild ID - Not 7 digits long');
+
+    this.id = id;
+
+    this.year = getYear(id);
+    this.school = getSchool(id);
+    this.uid = getUID(id);
+};
+
+module.exports.prototype.toString = function() {
+    return '[object StudentID]';
+};
+
+var getYear = function(id) {
+    return '20' + (id+'').substr(0,2);
+};
+
+var getSchool = function(id) {
+    var schoolId = parseInt((id+'').substr(2,1));
+
+    if(schoolId == 7)
+        return 'Deerpath Middle School';
+
+    if(schoolId == 8)
+        return 'Lake Bluff Middle School';
+
+    if(schoolId == 2)
+        return 'Lake Forest Country Day';
+
+    return 'Other School';
+};
+
+var getUID = function(id) {
+    return (id+'').substr(3,6);
+};
